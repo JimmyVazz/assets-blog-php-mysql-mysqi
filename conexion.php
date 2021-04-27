e2984d3fde0fb0130279a341c41943c97c653669
JimmyVazz <cjvasquez_98@hotmail.com>
2021-04-26 21:59:27 -0500

    se agregaron comentarios :sunglassess:

--- conexion.php
+++ conexion.php
@@ -1,17 +1,17 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', '', 'abarrotes');
+$mysqli = new mysqli('localhost', 'root', '', 'abarrotes'); //Objeto mysqli
 
-if($mysqli->connect_error){
+if($mysqli->connect_error){  //Si hubo algún error lanzamos con detalle 
     die("Error en la conexion" . $mysqli->connect_error);
 }else{
-    echo "<h1> Database connected </h1>";
-    
-    $SQL = "SELECT * FROM productos";
+    echo "<h1> Database connected </h1>"; // Si todo al 100%
 
-    if ($resultado = $mysqli->query($SQL)) {
+    $SQL = "SELECT * FROM productos"; //query
+
+    if ($resultado = $mysqli->query($SQL)) { //Si el query resulta OK, lo guardamos en resultado
         
-        while ($row = $resultado->fetch_assoc()) {
+        while ($row = $resultado->fetch_assoc()) { //Iteramos sobre cada fila, accediendo con el nombre del atributo
             echo "<p>" . $row["idProducto"]  . $row["nombreProducto"] . $row["precio"]
             . $row["fecha_caducidad"] . $row["stock"] . "</p>";           
             echo "<br>";
